227bda65634765c406bf4dbbb792ee210a818341
Peregrinus <zakharov.mark22@gmail.com>
2024-07-08 02:12:05 +0300

    feature: typesafe

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { ErrorBoundary } from './components/ErrorBoundary';
+import { ErrorButton } from './components/ErrorButton.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <ErrorButton message="Click me" />
+    </ErrorBoundary>
   </React.StrictMode>
 );
